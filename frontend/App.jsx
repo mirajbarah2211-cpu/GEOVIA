4ce8895e25d039489e7d44827bbe614e6b39eccb
mirajbarah2211-cpu <mirajbarah2211@gmail.com>
2025-10-28 09:25:30 +0300

    Add files via upload

---
+++ frontend/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{textAlign: 'center', marginTop: '50px'}}>
+      <h1>Welcome to GEOVIA!</h1>
+      <p>Dashboard, Projects, Tasks, AI, GIS Files</p>
+    </div>
+  );
+}
+
+export default App;
